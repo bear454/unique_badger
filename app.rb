368d7e7243e572a9b836dc9f4cb82b5826914234
James Mason <jmason@suse.com>
2015-04-21 14:15:49 -0700

    Skeleton

---
+++ app.rb
@@ -0,0 +1,35 @@
+# Copyright © 2015 SUSE LLC, James Mason <jmason@suse.com>.
+#
+# This file is part of UniqueBadger.
+#
+# UniqueBadger is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# UniqueBadger is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with UniqueBadger. If not, see <http://www.gnu.org/licenses/>.
+
+require 'sinatra/base'
+
+class UniqueBadger < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+
+  ENV['RACK_ENV'] ||= 'development'
+  require 'bundler'
+  Bundler.require :default, ENV['RACK_ENV'].to_sym
+
+  get '/' do
+    "UniqueBadger"
+  end
+
+  get '/*' do
+    status 404
+  end
+end
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,32 @@
+# Copyright © 2015 SUSE LLC, James Mason <jmason@suse.com>.
+#
+# This file is part of UniqueBadger.
+#
+# UniqueBadger is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# UniqueBadger is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with UniqueBadger. If not, see <http://www.gnu.org/licenses/>.
+
+
+ENV['RACK_ENV'] = 'test'
+ENV['FRAMEWORKS'] = File.expand_path(File.join(File.dirname(__FILE__), "fixtures/frameworks.xml"))
+
+require_relative File.join('..', 'app.rb')
+require 'rspec'
+require 'rack/test'
+
+RSpec.configure do |config|
+  include Rack::Test::Methods
+
+  def app
+    UniqueBadger
+  end
+end
